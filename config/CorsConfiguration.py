e7635c55bb5d70190d30ed4c5b5915d16aab8ec8
mariucaBianca <540269@student.fontys.nl>
2025-04-30 00:32:00 +0200

    Update CorsConfiguration.py

--- config/CorsConfiguration.py
+++ config/CorsConfiguration.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def configure_cors(app):
     origins = [
         "http://localhost:5173",
-        "https://read-safe-ai-frontend-oimz.vercel.app/"
+        "https://read-safe-ai-frontend-oimz.vercel.app"
     ]
 
     app.add_middleware(
